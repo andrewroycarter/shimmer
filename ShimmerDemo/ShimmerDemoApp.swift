0c07e40799049306defc1f3003c45abc266f28f1
Andrew Carter <andrew.carter@willowtreeapps.com>
2023-03-06 13:17:10 -0500

    Initial Commit

---
+++ ShimmerDemo/ShimmerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerDemoApp.swift
+//  ShimmerDemo
+//
+//  Created by Andrew Carter on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
